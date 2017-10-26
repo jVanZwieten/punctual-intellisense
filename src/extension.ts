a4f4f6e1dbd4537fb848517b3c6550bbece4c006
John VanZwieten <JVanZwieten@infragistics.com>
2017-10-26 02:23:25 -0400

    Abstracted activation keys into package.json, both in keybindings and configuration settings.

--- src/extension.ts
+++ src/extension.ts
@@ -2,6 +2,9 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode'
+import * as fs from 'fs'
+import * as process from 'process'
+import * as os from 'os'
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -9,18 +12,29 @@ export function activate(context: vscode.ExtensionContext) {
 
     // Use the console to output diagnostic information (console.log) and errors (console.error)
     // This line of code will only be executed once when your extension is activated
-    console.log('Congratulations, your extension "punctual-intellisense" is now active!');
 
     // The command has been defined in the package.json file
-    // Now provide the implementation of the command with  registerCommand
+    // Now provide the implementation of  the command with  registerCommand
     // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand('extension.acceptAndTypeForward', async () => {
+
+    registerTypeForwardCommands(context)
+
+    function registerTypeForwardCommands(context: vscode.ExtensionContext) {
+        const typeForwardKeys: string[] = vscode.workspace.getConfiguration().typeForwardKeys
+        typeForwardKeys.forEach(key => {
+            context.subscriptions.push(
+                vscode.commands.registerCommand(`extension.acceptAndType${key}`, () => acceptAndTypeForward(key))
+            )
+        });
+    }
+
+    async function acceptAndTypeForward(punctuation: string) {
         const editor = vscode.window.activeTextEditor
         const document = editor.document
 
         let suggestion = await getSuggestion()
         await addPunctiation()
-        await replaceFragmentWithSuggestion(suggestion)
+        replaceFragmentWithSuggestion(suggestion)
 
         async function getSuggestion() {
             await vscode.commands.executeCommand('acceptSelectedSuggestion')
@@ -33,7 +47,7 @@ export function activate(context: vscode.ExtensionContext) {
         }
 
         async function addPunctiation() {
-            await editor.edit(editBuilder => editBuilder.insert(cursorPosition(), '.'))
+            await editor.edit(editBuilder => editBuilder.insert(cursorPosition(), punctuation))
         }
 
         async function replaceFragmentWithSuggestion(suggestion) {
@@ -47,8 +61,46 @@ export function activate(context: vscode.ExtensionContext) {
             return editor.selection.active
         }
 
-    })
-    context.subscriptions.push(disposable)
+    }
+
+    // let disposable = vscode.commands.registerCommand('extension.acceptAndTypeForward', async (punct, another) => {
+    //     const punctuation = punct.context
+    //     const editor = vscode.window.activeTextEditor
+    //     const document = editor.document
+
+    //     let suggestion = await getSuggestion()
+    //     await addPunctiation()
+    //     await replaceFragmentWithSuggestion(suggestion)
+
+    //     async function getSuggestion() {
+    //         await vscode.commands.executeCommand('acceptSelectedSuggestion')
+
+    //         const suggestionRange = document.getWordRangeAtPosition(editor.selection.active)
+    //         const suggestion = editor.document.getText(suggestionRange)
+
+    //         await vscode.commands.executeCommand('undo')
+    //         return suggestion
+    //     }
+
+    //     async function addPunctiation() {
+    //         await editor.edit(editBuilder => editBuilder.insert(cursorPosition(), punctuation))
+    //     }
+
+    //     async function replaceFragmentWithSuggestion(suggestion) {
+    //         let positionBeforePunctuation = cursorPosition().translate(0, -1)
+    //         let draftRange = document.getWordRangeAtPosition(positionBeforePunctuation)
+    //             || new vscode.Range(positionBeforePunctuation, positionBeforePunctuation)
+    //         await editor.edit(editBuilder => editBuilder.replace(draftRange, suggestion))
+    //     }
+
+    //     function cursorPosition(): vscode.Position {
+    //         return editor.selection.active
+    //     }
+
+    // })
+    // context.subscriptions.push(disposable)
+
+    console.log('Congratulations, your extension "punctual-intellisense" is now active!');
 }
 
 // this method is called when your extension is deactivated
