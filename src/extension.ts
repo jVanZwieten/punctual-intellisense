cef6d524237bada0fe59079a1915ef0e1d57833c
John VanZwieten <JVanZwieten@infragistics.com>
2017-11-06 13:53:00 -0500

    abstracted key bindings into pacakge.json; extension now updates the keybindings on onDidChangeConfiguration according to typeForwardKeys in settings, which is now the one source of truth for accept keys

--- src/extension.ts
+++ src/extension.ts
@@ -1,24 +1,70 @@
 'use strict';
 import * as vscode from 'vscode'
+import * as fs from 'fs'
+import { shiftKeys } from "./shiftKeys"
 
-let disposables: vscode.Disposable[]=[]
+let disposables: vscode.Disposable[] = []
 
 export function activate(context: vscode.ExtensionContext) {
+    let typeForwardKeys: string[] = vscode.workspace.getConfiguration().typeForwardKeys
+    registerTypeForwardCommands(typeForwardKeys, context)
 
-
-    registerTypeForwardCommands(context)
     vscode.workspace.onDidChangeConfiguration(() => {
-        disposeCommands()
-        registerTypeForwardCommands(context)
+        let newTypeForwardKeys: string[] = vscode.workspace.getConfiguration().typeForwardKeys
+        if (!equivalent(typeForwardKeys, newTypeForwardKeys)) {
+            typeForwardKeys = newTypeForwardKeys
+
+            disposeCommands()
+            registerTypeForwardCommands(typeForwardKeys, context)
+            registerTypeForwardKeyBindings(typeForwardKeys, context)
+        }
     })
 
-    function registerTypeForwardCommands(context: vscode.ExtensionContext) {
-        const typeForwardKeys: string[] = vscode.workspace.getConfiguration().typeForwardKeys
-        typeForwardKeys.forEach(key => {
-            const disposable = vscode.commands.registerCommand(`extension.acceptAndType${key}`, () => acceptAndTypeForward(key))
+
+    function registerTypeForwardCommands(typeForwardKeys: string[], context: vscode.ExtensionContext) {
+        for (const key of typeForwardKeys) {
+            registerTypeForwardCommand(key, context)
+        }
+
+
+        function registerTypeForwardCommand(key: string, context: vscode.ExtensionContext) {
+            const disposable = vscode.commands.registerCommand(`extension.acceptAndType${key}`,
+                () => acceptAndTypeForward(key))
             context.subscriptions.push(disposable)
             disposables.push(disposable)
-        });
+        }
+    }
+
+    function registerTypeForwardKeyBindings(typeForwardKeys, context: vscode.ExtensionContext) {
+        const extensionSettingsPath = context.extensionPath + "\\package.json"
+        let extensionSettings = readExtensionSettings(extensionSettingsPath)
+
+        updateKeyBindings(extensionSettings, typeForwardKeys)
+        writeExtensionSettings(extensionSettingsPath, extensionSettings)
+
+
+        function updateKeyBindings(extensionSettings, typeForwardKeys: string[]) {
+            let keyBindings: KeyBinding[] = []
+            for (const key of typeForwardKeys)
+                keyBindings.push(new KeyBinding(key))
+
+            extensionSettings.contributes.keybindings = keyBindings
+        }
+
+        function readExtensionSettings(extensionSettingsPath: string): any {
+            try {
+                let settingsString = fs.readFileSync(extensionSettingsPath, 'utf8')
+                let settings = JSON.parse(settingsString)
+                return settings
+            } catch (error) {
+                console.log(error)
+            }
+        }
+
+        function writeExtensionSettings(extensionSettingsPath: string, settings) {
+            let saveSettings=JSON.stringify(settings)
+            fs.writeFileSync(extensionSettingsPath, JSON.stringify(settings))
+        }
     }
 
     async function acceptAndTypeForward(punctuation: string) {
@@ -56,11 +102,29 @@ export function activate(context: vscode.ExtensionContext) {
     }
 }
 
-    function disposeCommands(){
-        for(let command of disposables)
-            command.dispose()
-    }
-// this method is called when your extension is deactivated
+function disposeCommands() {
+    for (const command of disposables)
+        command.dispose()
+}
 export function deactivate() {
     disposeCommands()
+}
+
+function equivalent(array1: string[], array2: string[]): boolean {
+    return array1.length == array2.length && array1.every((v, i) => v === array2[i])
+}
+
+class KeyBinding {
+    command: string
+    key: string
+    when: string
+
+    constructor(key: string) {
+        let triggerKey
+        shiftKeys.hasOwnProperty(key) ? triggerKey = `shift+${shiftKeys[key]}` : triggerKey = key
+
+        this.command = `extension.acceptAndType${key}`
+        this.key = triggerKey
+        this.when = "editorTextFocus && suggestWidgetVisible"
+    }
 }
